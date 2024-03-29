2ccd445f23e4453f52bfc8b1e8a89293c1981cd4
raunakpurohit <raunak.wscubetech@gmail.com>
2024-03-29 20:07:15 +0530

    Database insert and fetch

--- lib/app_database.dart
+++ lib/app_database.dart
@@ -10,6 +10,12 @@ class AppDatabase{
   ///accessing the appdb class object
   static final AppDatabase db = AppDatabase._();
 
+  ///creating global static table name and fields
+  static const String TABLE_NAME_NOTE = "note";
+  static const String COLUMN_NOTE_ID = "note_id";
+  static const String COLUMN_NOTE_TITLE = "note_title";
+  static const String COLUMN_NOTE_DESC = "note_desc";
+
   /// all the logic of database will be provided here
 
   Database? myDb;
@@ -30,21 +36,44 @@ class AppDatabase{
     var rootPath = await getApplicationDocumentsDirectory();
     var actualPath = join(rootPath.path, "noteDB.db");
 
-    return await openDatabase(actualPath, version: 1, onCreate: (db, version){
+    return await openDatabase(actualPath, version: 1,
+        onCreate: (db, version){
       /// do that work which you want to execute in db when for the first time and only time when db is created..
       /// we need to add the tables in here..
 
-
+      db.execute("create table $TABLE_NAME_NOTE ( $COLUMN_NOTE_ID integer primary key autoincrement, $COLUMN_NOTE_TITLE text, $COLUMN_NOTE_DESC text)");
 
     });
 
   }
 
 
-  /// db create
-  /// table create
+  /// db create done
+  /// table create done
+
   /// insert data
+  void addNote({required String title, required String desc}) async{
+
+    var db = await getDb();
+
+    db.insert(TABLE_NAME_NOTE, {
+      COLUMN_NOTE_TITLE : title,
+      COLUMN_NOTE_DESC : desc,
+    });
+
+  }
+
+
   /// fetch data
+  Future<List<Map<String, dynamic>>> fetchAllNotes() async{
+    
+    var db = await getDb();
+    
+    var data = await db.query(TABLE_NAME_NOTE);
+
+    return data;
+    
+  }
 
 
 }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,20 +1,54 @@
 import 'package:database_239/app_database.dart';
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  AppDatabase? db;
+  List<Map<String, dynamic>> mData = [];
+
+  @override
+  void initState() {
+    super.initState();
+    db = AppDatabase.db; //accessing the database class object
+    getNotes();
+  }
+
+  void getNotes() async{
+    mData = await db!.fetchAllNotes();
+    setState(() {
+
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-
-    var db = AppDatabase.db; //accessing the database class object
-
     return Scaffold(
       appBar: AppBar(
         title: Text('Home'),
       ),
 
-      body: Container(),
+      body: mData.isNotEmpty ? ListView.builder(
+        itemCount: mData.length,
+          itemBuilder: (_, index){
+          return ListTile(
+            title: Text(mData[index][AppDatabase.COLUMN_NOTE_TITLE]),
+            subtitle: Text(mData[index][AppDatabase.COLUMN_NOTE_DESC]),
+          );
+      }) : Center(
+        child: Text('No Notes yet!!'),
+      ), ///fetch note from database
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){
+          ///add note into database
+          db!.addNote(title: "New Note", desc: "Do what makes you happy.");
+          getNotes();
+        },
+        child: Icon(Icons.add),
+      ),
     );
   }
 }
